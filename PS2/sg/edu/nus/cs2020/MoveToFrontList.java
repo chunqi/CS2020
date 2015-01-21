8ee9cf33df696c2f8940b93ebadec945b0eedbb8
Chunqi Zhu <chunqi93@gmail.com>
2015-01-21 15:40:44 +0800

    PS2 Initial Commit

---
+++ PS2/sg/edu/nus/cs2020/MoveToFrontList.java
@@ -0,0 +1,16 @@
+package sg.edu.nus.cs2020;
+/**
+ * MoveToFrontList
+ * Description: 
+ * CS2020 2012
+ * 
+ * You need to create a constructor and implement search.
+ */
+
+
+/**
+ * 
+ */
+public class MoveToFrontList extends FixedLengthList {
+
+}
